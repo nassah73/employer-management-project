66d343d1d0954b4929cac2cbfc96eec41413bd1b
hassan <hassansobai6@gmail.com>
2025-10-17 09:40:57 +0200

    modfing class gestion

--- GesionEntreprise.java
+++ GesionEntreprise.java
@@ -3,7 +3,7 @@
  import java.util.ArrayList;
  class GesionEntreprise {
     ArrayList <emploiyerPleinTemps>PleinTemps=new ArrayList<>();
-   
+    ArrayList <EmployeTempsPartiel>PartielTemps=new ArrayList<>();
      
    public  ArrayList <emploiyerPleinTemps> add_employer_plien_de_temp(emploiyerPleinTemps e){
      PleinTemps.add(e);
@@ -13,7 +13,25 @@
         PleinTemps.remove(e);
         return PleinTemps;
    }
- public void affich(){
+ 
+  public  ArrayList <EmployeTempsPartiel> add_employer_partiel_de_temp(EmployeTempsPartiel e){
+     PartielTemps.add(e);
+     return PartielTemps;
+   }
+  public ArrayList <EmployeTempsPartiel> remove_employer_partiel_temp(EmployeTempsPartiel e){
+        PartielTemps.remove(e);
+        return PartielTemps;
+   }
+ 
+ 
+ 
+     public void affich_PartialTemps(){
+   for (EmployeTempsPartiel emploi :PartielTemps ) {
+     System.out.println("Nom:"+ emploi.getName()+" age :"+emploi.getAge());
+   }
+ }
+ 
+   public void affich_PleinTemps(){
    for (emploiyerPleinTemps emploi :PleinTemps ) {
      System.out.println("Nom:"+ emploi.getName()+" age :"+emploi.getAge());
    }
